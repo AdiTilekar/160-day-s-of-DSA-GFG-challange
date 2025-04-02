21f57ad913a5dab41029f6e37aaef375c756bb79
Aditya Tilekar <144588554+AdiTilekar@users.noreply.github.com>
2025-04-02 17:35:53 +0530

    Day2.py

--- Day2.py
+++ Day2.py
@@ -1,7 +1,7 @@
 # Day 2 :- Move all zeros to end
 class Solution:
 	def pushZerosToEnd(self,arr):
-	    j =c 0
+	    j = 0
 	    for i in range(len(arr)):
 	        if arr[i]!=0:
 	            arr[i],arr[j] = arr[j],arr[i]
