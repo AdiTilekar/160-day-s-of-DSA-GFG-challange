a87870498d7d9a6edbd06a3389fa715dac06df10
Aditya Tilekar <144588554+AdiTilekar@users.noreply.github.com>
2025-04-02 15:55:59 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getSecondLargest(self, arr):
+        arr=list(set(arr))
+        if len(arr) < 2:
+            return -1
+        arr.sort(reverse=True)
+        return arr[1]
